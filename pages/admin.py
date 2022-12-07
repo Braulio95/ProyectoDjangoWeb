1a84083a82ce16a28a9a514abcf3561d2e4f294f
Braulio95 <braulioestradaalfonso@hotmail.com>
2022-12-07 12:23:23 -0600

    se agrega el proyecto

---
+++ pages/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Page
+
+#Configuracion extra del modelo
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at','updated_at')
+    search_fields = ('title','content')
+    list_filter = ('visible',)
+    list_display = ('title','visible','created_at')
+    ordering = ('created_at',)
+
+# Register your models here.
+admin.site.register(Page, PageAdmin)
+
+
+title = "Proyecto Braulio"
+subtitle = "Panel de Gestión"
+
+admin.site.site_header = title
+admin.site.site_title = title
+admin.site.index_title = subtitle
